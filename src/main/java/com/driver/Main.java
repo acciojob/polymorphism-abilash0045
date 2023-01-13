f2d97c01ea61a958a66d9f035061483d4f7110b9
Abilash S L <abilash0045@gmail.com>
2023-01-13 14:03:13 +0530

    soln2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,13 +3,13 @@ package com.driver;
 public class Main {
     public static class Product{
         public int product(int x, int y) {
-            return x+y;
+            return 0;
         }
         public int product(int x, int y, int z) {
-            return x + y + z;
+            return 1;
         }
         public double product(double x, double y) {
-            return x + y;
+            return 2;
         }
     }
 
